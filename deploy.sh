19c4feab8d7664c2abe1d756c18555ec4a826b9b
el-mardi <el.ayoub.97@gmail.com>
2022-03-08 19:01:59 +0100

    edit styling

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy to Github pages'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:el-mardi/Portfolio-with-vue-js-3.git main:gh-pages
+git push -f git@github.com:el-mardi/Portfolio-with-vue-js-3.git master:gh-pages
 
 cd -
